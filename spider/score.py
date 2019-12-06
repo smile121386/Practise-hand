e0457b49e1f8282fb4e55c3ef212291de1b7e141
smile121386 <1030651648@qq.com>
2019-12-06 10:31:14 +0800

    2019.12.6

--- spider/score.py
+++ spider/score.py
@@ -5,9 +5,11 @@ with open('html.html', 'r', encoding='utf-8') as f:
     html = f.read()
 pattern = re.compile('<tr(.*?)</tr>', re.S)
 results = re.findall(pattern, html)
-wb = openpyxl.Workbook('test.xlsx')
+wb = openpyxl.Workbook()
 ws = wb.active
-ws.title = "WorkSheetTitle"
+ws.title = 'aa'
+print(wb.sheetnames)
+
 
 
 del results[0]
@@ -16,10 +18,10 @@ for i in results:
 
     p = re.compile('<td>(.*?)</td>', re.S)
     w = re.findall(p, i)
-    print('学科：%s，成绩：%s' % (w[1], w[4]))
+    # print('学科：%s，成绩：%s' % (w[1], w[4]))
     c = ws.cell(row=1,column=j,value=w[1])
     d = ws.cell(row=2,column=j,value=w[4])
     j += 1
 
-
-print(len(results))
+wb.save('b.xlsx')
+# print(len(results))
